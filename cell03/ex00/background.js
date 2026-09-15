30f8835c970a241f5e7faf28b4adfc12dc135e95
chiqq <nueh2549@gmail.com>
2026-09-16 01:53:30 +0700

    feat many many ex

---
+++ cell03/ex00/background.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("changeButton");
+
+button.addEventListener("click", function () {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    const randomColor = "rgb(" + red + ", " + green + ", " + blue + ")";
+
+    document.body.style.backgroundColor = randomColor;
+});
