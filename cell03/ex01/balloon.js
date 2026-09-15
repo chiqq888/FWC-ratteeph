ae8930f0494de370cb754d08860b90ab2287e0c3
chiqq <nueh2549@gmail.com>
2026-09-16 02:47:13 +0700

    feat: add balloon animation and interaction functionality

---
+++ cell03/ex01/balloon.js
@@ -0,0 +1,43 @@
+const balloon = document.getElementById("balloon");
+
+let size = 200;
+
+const colors = ["red", "green", "blue"];
+let colorIndex = 0;
+
+balloon.addEventListener("click", function () {
+    size = size + 10;
+
+    if (size > 420) {
+        size = 200;
+    }
+
+    colorIndex = colorIndex + 1;
+
+    if (colorIndex > 2) {
+        colorIndex = 0;
+    }
+
+    updateBalloon();
+});
+
+balloon.addEventListener("mouseleave", function () {
+    /* ลดขนาด แต่ห้ามต่ำกว่า 200px */
+    if (size > 200) {
+        size = size - 5;
+    }
+
+    colorIndex = colorIndex - 1;
+
+    if (colorIndex < 0) {
+        colorIndex = 2;
+    }
+
+    updateBalloon();
+});
+
+function updateBalloon() {
+    balloon.style.width = size + "px";
+    balloon.style.height = size + "px";
+    balloon.style.backgroundColor = colors[colorIndex];
+}
